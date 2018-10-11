12eaeed3f4808a54202303c4f2c52b8a2a16a535
JoshHC <davidhigueros@gmail.com>
2018-10-10 20:19:38 -0600

    Prueba en Nexus 6 Y Arreglo de HardCoded Text

--- CifradoC/app/src/main/java/com/example/josue/cifrado/MainActivity.java
+++ CifradoC/app/src/main/java/com/example/josue/cifrado/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -139,6 +140,9 @@ public class MainActivity extends AppCompatActivity
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
 
         if (id == R.id.nav_camera) {
+            TextView Instrucciones = findViewById(R.id.textinstrucciones);
+            Instrucciones.setEnabled(false);
+            Instrucciones.setVisibility(View.INVISIBLE);
             fragmentManager.beginTransaction().replace(R.id.contenedor, new ProcesoCifradoZigZag()).commit();
         } else if (id == R.id.nav_gallery) {
             fragmentManager.beginTransaction().replace(R.id.contenedor, new CifradoEspiral()).commit();
